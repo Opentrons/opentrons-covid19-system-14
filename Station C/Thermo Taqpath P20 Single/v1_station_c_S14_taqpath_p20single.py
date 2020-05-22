06fb2f7b930dcde919a133120edebc293baa4acb
Nick Diehl <ndiehl@opentrons.com>
2020-05-22 10:52:34 -0400

    initial station C Thermo Taqpath

--- Station C/Thermo Taqpath P20 Single/v1_station_c_S14_taqpath_p20single.py
+++ Station C/Thermo Taqpath P20 Single/v1_station_c_S14_taqpath_p20single.py
@@ -1,17 +1,18 @@
 from opentrons import protocol_api
 import json
 import os
+import math
 
 # metadata
 metadata = {
-    'protocolName': 'Version 1 S14 Station C Viasure P20 Single',
+    'protocolName': 'Version 1 S14 Station C Thermo Taqpath P20 Single',
     'author': 'Nick <protocols@opentrons.com>',
     'source': 'Custom Protocol Request',
     'apiLevel': '2.3'
 }
 
 NUM_SAMPLES = 8  # start with 8 samples, slowly increase to 48, then 94 (max is 94)
-SAMPLE_VOL = 5
+SAMPLE_VOL = 10
 PREPARE_MASTERMIX = True
 TIP_TRACK = False
 
@@ -88,51 +89,54 @@ resuming.')
     mm_tube = tube_block.wells()[0]
     mm_dict = {
         'volume': 15,
-        'components': {}
+        'components': {
+            tube: vol for tube, vol in zip(tube_block.columns()[1][:3],
+                                           [6.25, 1.25, 7.5])
+        }
     }
 
-    # total_mm_vol = mm_dict['volume']*(NUM_SAMPLES+2)*1.15
-    # # translate total mastermix volume to starting height
-    # r = mm_tube.diameter/2
-    # mm_height = total_mm_vol/(math.pi*(r**2)) - 5
-    #
-    # def h_track(vol):
-    #     nonlocal mm_height
-    #     dh = 1.1*vol/(math.pi*(r**2))  # compensate for 10% theoretical volume loss
-    #     mm_height = mm_height - dh if mm_height - dh > 2 else 2  # stop at 2mm above mm tube bottom
-    #     return mm_tube.bottom(mm_height)
-    #
-    # if PREPARE_MASTERMIX:
-    #     vol_overage = 1.2 if NUM_SAMPLES > 48 else 1.1  # decrease overage for small sample number
-    #
-    #     for i, (tube, vol) in enumerate(mm_dict['components'].items()):
-    #         comp_vol = vol*(NUM_SAMPLES+2)*vol_overage
-    #         pip = p300 if comp_vol > 20 else p20
-    #         pick_up(pip)
-    #         num_trans = math.ceil(comp_vol/160)
-    #         vol_per_trans = comp_vol/num_trans
-    #         for _ in range(num_trans):
-    #             pip.air_gap(20)
-    #             pip.aspirate(vol_per_trans, tube)
-    #             ctx.delay(seconds=3)
-    #             pip.touch_tip(tube)
-    #             pip.air_gap(20)
-    #             pip.dispense(20, mm_tube.top())  # void air gap
-    #             pip.dispense(vol_per_trans, mm_tube.bottom(2))
-    #             pip.dispense(20, mm_tube.top())  # void pre-loaded air gap
-    #             pip.blow_out(mm_tube.top())
-    #             pip.touch_tip(mm_tube)
-    #         if i < len(mm_dict['components'].items()) - 1 or pip == p20:  # only keep tip if last component and p300 in use
-    #             pip.drop_tip()
-    #     mm_total_vol = mm_dict['volume']*(NUM_SAMPLES+2)*vol_overage
-    #     if not p300.hw_pipette['has_tip']:  # pickup tip with P300 if necessary for mixing
-    #         pick_up(p300)
-    #     mix_vol = mm_total_vol / 2 if mm_total_vol / 2 <= 200 else 200  # mix volume is 1/2 MM total, maxing at 200µl
-    #     mix_loc = mm_tube.bottom(20) if NUM_SAMPLES > 48 else mm_tube.bottom(5)
-    #     p300.mix(7, mix_vol, mix_loc)
-    #     p300.blow_out(mm_tube.top())
-    #     p300.touch_tip()
-    #     p300.drop_tip()
+    vol_overage = 1.2 if NUM_SAMPLES > 48 else 1.1  # decrease overage for small sample number
+    total_mm_vol = mm_dict['volume']*(NUM_SAMPLES+2)*vol_overage
+    # translate total mastermix volume to starting height
+    r = mm_tube.diameter/2
+    mm_height = total_mm_vol/(math.pi*(r**2)) - 5
+
+    def h_track(vol):
+        nonlocal mm_height
+        dh = 1.1*vol/(math.pi*(r**2))  # compensate for 10% theoretical volume loss
+        mm_height = mm_height - dh if mm_height - dh > 2 else 2  # stop at 2mm above mm tube bottom
+        return mm_tube.bottom(mm_height)
+
+    if PREPARE_MASTERMIX:
+
+        for i, (tube, vol) in enumerate(mm_dict['components'].items()):
+            comp_vol = vol*(NUM_SAMPLES+2)*vol_overage
+            pip = p300 if comp_vol > 20 else p20
+            pick_up(pip)
+            num_trans = math.ceil(comp_vol/160)
+            vol_per_trans = comp_vol/num_trans
+            for _ in range(num_trans):
+                pip.air_gap(20)
+                pip.aspirate(vol_per_trans, tube)
+                ctx.delay(seconds=3)
+                pip.touch_tip(tube)
+                pip.air_gap(20)
+                pip.dispense(20, mm_tube.top())  # void air gap
+                pip.dispense(vol_per_trans, mm_tube.bottom(2))
+                pip.dispense(20, mm_tube.top())  # void pre-loaded air gap
+                pip.blow_out(mm_tube.top())
+                pip.touch_tip(mm_tube)
+            if i < len(mm_dict['components'].items()) - 1 or pip == p20:  # only keep tip if last component and p300 in use
+                pip.drop_tip()
+        mm_total_vol = mm_dict['volume']*(NUM_SAMPLES+2)*vol_overage
+        if not p300.hw_pipette['has_tip']:  # pickup tip with P300 if necessary for mixing
+            pick_up(p300)
+        mix_vol = mm_total_vol / 2 if mm_total_vol / 2 <= 200 else 200  # mix volume is 1/2 MM total, maxing at 200µl
+        mix_loc = mm_tube.bottom(20) if NUM_SAMPLES > 48 else mm_tube.bottom(5)
+        p300.mix(7, mix_vol, mix_loc)
+        p300.blow_out(mm_tube.top())
+        p300.touch_tip()
+        p300.drop_tip()
 
     # transfer mastermix to TD plate
     mm_vol = mm_dict['volume']
